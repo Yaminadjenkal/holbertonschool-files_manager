cc66f5bc92b4d50befd072b3073eeb294de85e32
Yaminadjenkal <mina20101951@hotmail.com>
2026-08-14 11:17:09 +0200

    test

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,4 +1,5 @@
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 import sha1 from 'sha1';
 
 class UsersController {
@@ -29,6 +30,34 @@ class UsersController {
     return res.status(201).json({
       id: result.insertedId,
       email,
+    });
+  }
+
+  static async getMe(req, res) {
+    const token = req.headers['x-token'];
+
+    if (!token) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+
+    if (!userId) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    const user = await dbClient.db.collection('users').findOne({
+      _id: dbClient.getObjectId(userId),
+    });
+
+    if (!user) {
+      return res.status(401).json({ error: 'Unauthorized' });
+    }
+
+    return res.status(200).json({
+      id: user._id.toString(),
+      email: user.email,
     });
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,12 @@
 import express from 'express';
-import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
 
 const router = express.Router();
 
-router.get('/status', AppController.getStatus);
-router.get('/stats', AppController.getStats);
-
-// Holberton tests use GET instead of POST
 router.post('/users', UsersController.postNew);
-router.get('/users', UsersController.postNew);
+router.get('/connect', AuthController.getConnect);
+router.get('/disconnect', AuthController.getDisconnect);
+router.get('/users/me', UsersController.getMe);
 
 export default router;
