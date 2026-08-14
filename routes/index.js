66dff39787993abc3cd9155327c7daac580ad9c0
Yaminadjenkal <mina20101951@hotmail.com>
2026-08-14 21:29:10 +0200

    Fix routes index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 import express from 'express';
+import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
 import FilesController from '../controllers/FilesController';
 
 const router = express.Router();
+
+// App
+router.get('/status', AppController.getStatus);
+router.get('/stats', AppController.getStats);
 
 // Users
 router.post('/users', UsersController.postNew);
